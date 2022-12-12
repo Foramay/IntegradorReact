3800b134998a18b3083571ef1c2637dea354995f
David Forastier <davidforastier10@gmail.com>
2022-12-12 05:23:21 -0300

    Lenguaje de busqueda en espalol

--- my-app/src/service/noticiasJson.js
+++ my-app/src/service/noticiasJson.js
@@ -4,7 +4,7 @@ const API_KEY = '6557b35dc78a47fc97d85d390910220d'
 //https://newsapi.org/v2/everything?q=bitcoin&apiKey=6557b35dc78a47fc97d85d390910220d&page=1&pageSize=10
 
 export const getNoticiasServer = async (buscadorNoticias, paginaActual) => {
-    const respuesta = await fetch(`${NEWS_API}/v2/everything?q=${buscadorNoticias}&apiKey=${API_KEY}&page=${paginaActual}&pageSize=10`);
+    const respuesta = await fetch(`${NEWS_API}/v2/everything?q=${buscadorNoticias}&apiKey=${API_KEY}&page=${paginaActual}&pageSize=10&language=es`);
     const noticias = await respuesta.json();
     return noticias;
 }
