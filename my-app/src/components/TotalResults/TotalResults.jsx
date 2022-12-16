c803e64d91efbc14e1a1441b02cf742c3d22f94a
David Forastier <davidforastier10@gmail.com>
2022-12-16 08:06:42 -0300

    El total de resultados de busqueda

---
+++ my-app/src/components/TotalResults/TotalResults.jsx
@@ -0,0 +1,8 @@
+const TotalResults = ({resultados}) => {
+    return (
+        <div>El total de resultados de la busqeuda es de {resultados}</div>
+    )
+}
+
+
+export default TotalResults;
--- my-app/src/pages/PaginaNoticias.jsx
+++ my-app/src/pages/PaginaNoticias.jsx
@@ -8,6 +8,7 @@ import { getNoticiasServer } from "../service/noticiasJson";
 import './PaginaNoticia.css';
 import { useSearchParams } from 'react-router-dom';
 import { useEffect } from "react";
+import TotalResults from "../components/TotalResults/TotalResults";
 
 const PaginaNoticias = () =>{
   const [noticias, setNoticias] = useState();
@@ -15,6 +16,8 @@ const PaginaNoticias = () =>{
   const [cantidadPaginas, setCantidadPaginas] = useState(1);
   const [searchParams, setSearchParams] = useSearchParams();
   const [pagina, setPagina] = useState();
+  //Definí un estado para poder saber el total de resultados
+  const [totalResultados, setTotalResultados] = useState();
  
   useEffect(() => {
     if(searchParams.get('query')){
@@ -28,7 +31,9 @@ const PaginaNoticias = () =>{
     setNoticias(news);
     setCantidadPaginas(Math.ceil(parseInt(totalResults)/10));
     setIsLoading(false);
-  }
+    //Acá paso el total de resultados
+    setTotalResultados(totalResults);
+  } 
 
   const onBuscar = (buscadorNoticias) => {
     setSearchParams({query : buscadorNoticias});
@@ -42,6 +47,8 @@ const PaginaNoticias = () =>{
               <Navbar/>
               <Buscador onBuscar={onBuscar}/>
               {isLoading && <Loading/>}
+              {/*Acá digo que si hay noticias muestro el resultado total */}
+              {noticias && <TotalResults resultados={totalResultados}/>}
               {noticias && <NoticiaList noticias={noticias}/>}
               {noticias && <PaginationOutlined cantidadPaginas={cantidadPaginas} onChange={onCambioPagina}/>}
           </main>
